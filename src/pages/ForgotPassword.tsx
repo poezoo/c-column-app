14812e8a0244891c66debceed7af926086fac7fb
poezoo <poezooletter@gmail.com>
2021-02-06 23:16:40 +0900

    パスワード再発行・プロフィール更新ページの追加

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,28 +1,26 @@
 import React, { useRef, useState } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
-// import { Formik, Field, Form, FormikHelpers } from 'formik';
 
 export const ForgotPassword: React.FC = () => {
-  const history = useHistory();
   const emailRef = useRef<HTMLInputElement>();
-  const passwordRef = useRef<HTMLInputElement>();
-  const { login, currentUser } = useAuth();
+  const { resetPassword } = useAuth();
   const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
   const [error, setError] = useState('');
 
   async function handleSubmit(e) {
-    // e.preventDefault();
-    // try {
-    //   setError('');
-    //   setLoading(true);
-    //   await login(emailRef.current.value, passwordRef.current.value);
-    //   history.push('/');
-    // } catch {
-    //   setError('Failed to log in');
-    // }
-    // setLoading(false);
+    e.preventDefault();
+    try {
+      setError('');
+      setLoading(true);
+      await resetPassword(emailRef.current.value);
+      setMessage('Check your inbox for further instructions');
+    } catch {
+      setError('Failed to reset');
+    }
+    setLoading(false);
   }
 
   return (
@@ -30,9 +28,9 @@ export const ForgotPassword: React.FC = () => {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">パスワード再発行</h2>
-          {currentUser && currentUser.email}
           {error && <Alert variant="danger">{error}</Alert>}
-          <Form>
+          {message && <Alert variant="success">{message}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>メールアドレス</Form.Label>
               <Form.Control type="email" ref={emailRef} required></Form.Control>
@@ -41,8 +39,14 @@ export const ForgotPassword: React.FC = () => {
               パスワード再発行
             </Button>
           </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/login">ログイン</Link>
+          </div>
         </Card.Body>
       </Card>
+      <div className="w-100 text-center mt-2">
+        アカウントが必要ですか? <Link to="/signup">アカウント登録</Link>
+      </div>
     </section>
   );
 };
--- src/pages/UpdateProfile.tsx
+++ src/pages/UpdateProfile.tsx
@@ -1,37 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import { Button, Card, Alert } from 'react-bootstrap';
+import { Button, Card, Alert, Form } from 'react-bootstrap';
 
 export const UpdateProfile: React.FC = () => {
-  const { updateEmail, updatePassword, currentUser } = useAuth();
-  const history = useHistory();
+  const emailRef = useRef<HTMLInputElement>();
+  const passwordRef = useRef<HTMLInputElement>();
+  const passwordConfirmRef = useRef<HTMLInputElement>();
+  const { currentUser, updatePassword, updateEmail } = useAuth();
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
-  const handleUpdateProfile = () => {
-    // try {
-    //   setError('');
-    //   setLoading(true);
-    // } catch {
-    //   setError('Failed to logout');
-    // }
-    // history.push('/');
-    // setLoading(false);
-  };
+  const history = useHistory();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Passwords do not match');
+    }
+
+    const promises = [];
+    setLoading(true);
+    setError('');
+
+    if (emailRef.current.value !== currentUser.email) {
+      promises.push(updateEmail(emailRef.current.value));
+    }
+    if (passwordRef.current.value) {
+      promises.push(updatePassword(passwordRef.current.value));
+    }
+
+    Promise.all(promises)
+      .then(() => {
+        history.push('/');
+      })
+      .catch(() => {
+        setError('Failed to update account');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
   return (
-    <section>
+    <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">プロフィール変更</h2>
+          <h2 className="text-center mb-4">プロフィール更新</h2>
           {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email: {currentUser.email}</strong>
-          <div className="w-100 text-center mt-2">
-            <Button onClick={handleUpdateProfile} disabled={loading}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>メールアドレス</Form.Label>
+              <Form.Control type="email" ref={emailRef} required defaultValue={currentUser.email} />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>パスワード</Form.Label>
+              <Form.Control type="password" ref={passwordRef} placeholder="変更しない場合は空のままにしてください" />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordConfirmRef}
+                placeholder="変更しない場合は空のままにしてください"
+              />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
               更新する
             </Button>
-          </div>
+          </Form>
         </Card.Body>
       </Card>
-    </section>
+      <div className="w-100 text-center mt-2">
+        <Link to="/">更新しない</Link>
+      </div>
+    </>
   );
 };
