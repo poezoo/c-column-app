d2a5c141b877cc6cf04813e81d97a2e6a4a11422
poezoo <poezooletter@gmail.com>
2021-02-07 18:29:08 +0900

    カラーパレット用ファイル追加

---
+++ src/components/commons/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  BLACK: '#333',
+  DARKGRAY: '#666666',
+  GRAY: '#DBD8D8',
+  LIGHTGRAY: '#EFEFEF',
+  PALEGRAY: '#EDEAEA',
+  BLUE: '#1BB8CE',
+  LIGHTBLUE: '#2BCCD8',
+  CREAM: '#FAF9F4',
+  LEMON: '#FFF384',
+  ORANGE: '#F3A367',
+  WHITE: '#FFFFFF',
+};
+
+export default colors;
