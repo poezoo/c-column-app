9e3e45c3eb22aafab6f21fae9fc99f3270418aa6
poezoo <poezooletter@gmail.com>
2021-02-09 01:32:09 +0900

    Imageファイルの追加

---
+++ src/images/index.ts
@@ -0,0 +1,9 @@
+import img_column from './img_column.png';
+import img_night_sky from './img_night_sky.png';
+
+const images = {
+  img_column,
+  img_night_sky,
+};
+
+export default images;
--- src/pages/Column.tsx
+++ src/pages/Column.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
 import { Button } from '../components/commons/Button';
-export const Column: React.FC = () => (
-  <section>
-    <h1>column page</h1>
-    <Button variant="primary">テスト</Button>
-  </section>
-);
+import images from '../images/index';
+export const Column: React.FC = () => {
+  return (
+    <section>
+      <h1>column page</h1>
+      <div>
+        <img src={images.img_column} />
+        <img src={images.img_night_sky} />
+      </div>
+
+      <Button variant="primary">テスト</Button>
+    </section>
+  );
+};
